86cffab2ec2eea0e09a833316d7d9d6a74f608cd
GGmaz <kajtes.nikola@gmail.com>
2023-01-22 22:31:50 +0100

    check does user exist in uns db

--- ftn/professors/views.py
+++ ftn/professors/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Professor
+import json
+import requests
 
 
 # Create your views here.
+
+def checkIfProfessorNotExistsInUnsDb(form):
+    url = 'http://localhost:8050/api/professor/'
+    data = {
+        'jmbg': form.cleaned_data['jmbg'],
+        'first_name': form.cleaned_data['first_name'],
+        'last_name': form.cleaned_data['last_name']
+    }
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    if response.status_code == 201:
+        return True
+    return False
+
 
 def ProfessorRegistrationView(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            if checkIfProfessorNotExistsInUnsDb(form):
+                form.save()
+            else:
+                return render(request, 'professors/register.html', {'form': form, 'error': 'Professor exists already in UNS database'})
             return redirect('professor-list')
     else:
         form = RegistrationForm()
--- ftn/students/views.py
+++ ftn/students/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Student
+import json
+import requests
 
 
 # Create your views here.
+
+def checkIfStudentNotExistsInUnsDb(form):
+    url = 'http://localhost:8050/api/student/'
+    data = {
+        'jmbg': form.cleaned_data['jmbg'],
+        'first_name': form.cleaned_data['first_name'],
+        'last_name': form.cleaned_data['last_name'],
+        'index': form.cleaned_data['index']
+    }
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    if response.status_code == 201:
+        return True
+    return False
+
 
 def StudentRegistrationView(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            if checkIfStudentNotExistsInUnsDb(form):
+                form.save()
+            else:
+                return render(request, 'students/register.html', {'form': form, 'error': 'Student exists already in UNS database'})
             return redirect('student-list')
     else:
         form = RegistrationForm()
